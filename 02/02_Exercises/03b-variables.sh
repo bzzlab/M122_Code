c92b3e82b3b8e90535b272a7fa6f92c74fa6bedc
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2022-11-25 13:51:02 +0100

    02 crlf ->lf

--- 02/02_Exercises/03b-variables.sh
+++ 02/02_Exercises/03b-variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Remark: This script works only in Ubuntu.
-# Therefore copy 06a-source.sh and this script with docker cp
+# Therefore copy 03a-source.sh and this script with docker cp
 # Example
 # 1. fetch container-id with:
 # docker ps
 # 2. copy host-file to destination container and path
-# docker cp ./06a-source.sh <container-id>:/tmp
+# docker cp ./03a-source.sh <container-id>:/tmp
 # Variables
 COURSE="Modul 122 - Abläufe mit Scriptsprachen automatisieren"
 SITE="ict.bzzlab.ch"
