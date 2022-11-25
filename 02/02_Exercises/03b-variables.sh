4ad00c7f9311e722abe3120451c59d84eb49070e
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2022-11-25 16:50:31 +0100

    Update 03b-variables.sh

--- 02/02_Exercises/03b-variables.sh
+++ 02/02_Exercises/03b-variables.sh
@@ -13,5 +13,5 @@ SITE="ict.bzzlab.ch"
 # Functions
 function gethostdetails {
   hostname
-  ip -a | grep -v inet6 | grep inet | awk '{print $2}' | grep -v ^127
+  ip address | grep -v inet6 | grep inet | awk '{print $2}' | grep -v ^127
 }
